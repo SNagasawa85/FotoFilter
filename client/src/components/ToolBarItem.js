92e0d9c571a83e657767dea066a1fd458c4b8a14
Jason Frantz <jason.frantz@hotmail.com>
2022-07-21 17:24:19 -0600

    EditPage commit

--- client/src/components/ToolBarItem.js
+++ client/src/components/ToolBarItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import styles from '../styles/toolbar.module.css';
+
 export default ({name, active, handleClick}) => {
 
 
 return(
-    <button className={`toolbar-item ${active ? 'active' : '' }`} onClick={ handleClick }>
+    <button className={`toolbar-item ${active ? 'active' : '' } ${styles.toolbarItem}`} onClick={ handleClick }>
         {name}
     </button>
     )
